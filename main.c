38e37f8092592f3201a6ecb41cc47f0296a56696
Benjamin Goihman <bengoihman@gmail.com>
2024-11-25 14:49:48 -0500

    vsocde ignore

--- main.c
+++ main.c
@@ -6,15 +6,20 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+int get_rand(){
+    int r = -1;
+    int file = open("/dev/urandom",O_RDONLY,0);
+    int r_file = read(file,&r,4);
+    if (r < 0){r*=-1;}
+    r%=5+1;
+    return r;
+}
 int main()
 {
     // pid_t p1;
     // p1 = fork();
     int r = -1;
     // pid_t p2 = fork();
-    int file = open("/dev/urandom",O_RDONLY,0);
-    int r_file = read(file,&r,4);
-    if (r < 0){r*=-1;}
-    r%=5+1;
+
     printf("%d\n",r);
 }
