cb327e3c35a7f083615ec009c8316f0b42b3368a
Benjamin Goihman <bengoihman@gmail.com>
2024-11-26 14:21:21 -0500

    start code

--- main.c
+++ main.c
@@ -10,6 +10,7 @@ int get_rand(){
     int r = -1;
     int file = open("/dev/urandom",O_RDONLY,0);
     int r_file = read(file,&r,4);
+    close(file);
     if (r < 0){r*=-1;}
     r%=5+1;
     return r;
@@ -18,9 +19,23 @@ int main()
 {
     pid_t p1;
     pid_t p2;
-    p1 = fork();
-    int r = -1;
-    pid_t p2 = fork();
+    // int t1,t2;
+    // t1 = get_rand();
+    // t2 = get_rand();
+    ((p1=fork())&&(p2=fork()));
+    if(p1<0||p2<0){
+        perror("fork fail ):");//output to stderr instead of stdout
+        exit(1);
+    }
+    else if(p1==0||p2==0){
+        int t1 = get_rand();
+        printf("%d %dsec",getpid(),t1);
+        waitpid(getpid(),&t1,0);
+    }
+    else{
+        printf("%d about to create 2 child process",getpid());
+
+    }
 
     
 }
